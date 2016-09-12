cb1334727916f9b9f398f29493847c9169d0793a
Alexander Nesterenko <nestorf250@gmail.com>
2016-09-12 23:28:14 +0300

    Finally, finish implementation of script

--- diff.py
+++ diff.py
@@ -1,28 +1,31 @@
 def strcmp(first, second):
     same_chars = 0
     sign = 1
-    mark = False
-    checkpoints = []
-    prev_char = ''
+    processing_index = -1
+    current_count_progress = 0
     if len(first) > len(second):
         sign = -1
         first, second = second, first
-    for to_chr in second:
-        found = False
-        if prev_char == to_chr:
-            continue
-        prev_char = to_chr
-        # Problem in this part.
-        # The position of current element should be saved and checked
-        for from_chr in first:
-            if to_chr == from_chr:
-                found = True
-                mark = True
-                same_chars += 1
+    for i, to_chr in zip(range(len(second)), second):
+        if processing_index < 0: 
+            for j, from_chr in zip(range(len(first)), first):
+                if to_chr == from_chr:
+                    current_count_progress = 1
+                    processing_index = j
+                    break
+        else:
+            processing_index += 1
+            if processing_index >= len(first):
                 break
-        if mark and not found:
-            mark = False
-            same_chars -= 1
+            if to_chr == first[processing_index]:
+                current_count_progress += 1
+            else:
+                if current_count_progress > 1:
+                    same_chars += current_count_progress
+                processing_index = -1
+                current_count_progress = 0
+    if current_count_progress:
+        same_chars += current_count_progress
     return sign * (1 - float(same_chars) / len(second))
         
 if __name__ == '__main__':
@@ -32,10 +35,10 @@ if __name__ == '__main__':
         (('a', 'aa'), 50),
         (('ab', 'ab'), 0),
         (('ab', 'aaab'), 50),
-        (('ba', 'abcd'), 100),
+        (('ba', 'gjhghj'), 100),
         (('ba', 'babc'), 50),
         (('aa', 'baab'), 50),
-        (('babc', 'ab'), -50),
+        (('babc', 'ab'), -75),
     )
 
     for strs, assertion in tests:
